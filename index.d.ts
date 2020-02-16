4ff75ab5edc392fc382e60d55d5ff3f56dff35b3
Drazail <30296570+Drazail@users.noreply.github.com>
2020-02-15 22:35:28 -0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,8 @@
-/*
-import RNHash from ".";
 
-export as namespace RNHash;
+import RNBytes from ".";
 
-export function hashFile(uri: string, algorithm: string):Promise<string>;
-export function hashUrl(url: string, HTTPMethod: string, headers: Record<string, string>, algorithm: string):Promise<string>;
-export function hashString(message: string, algorithm: string):Promise<string>;
-export function generateHmac(message: string, key: string, algorithm: string):Promise<string>;
+export as namespace RNBytes;
 
-export namespace Constants {
-    export const HashAlgorithms : Record<string, string>;
-    export const HmacAlgorithms : Record<string, string>;
-}
-
-export namespace JSHash {
-    export function hashString(message: string, algorithm: string):Promise<string>;
-}
-*/
+export function readFromAndWriteTo(sourcePath : string, targetPath: string, shouldOverWrite: boolean, shoudlAppend: boolean, FirstByteIndex: number, finalByteIndex: number): Promise<string>
+export function rm(sourcePath: string):Promise<string>;
+export function getFileLength(path: string):Promise<string>;
